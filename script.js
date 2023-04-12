e7789c55d72fbd85d0cd52ce7e18f410b41793b3
sureshvaddegithub <116425651+sureshvaddegithub@users.noreply.github.com>
2023-04-12 10:59:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function fibonacci(num) {
 // your code here
+	if(num == 1){
+		return 0;
+	}
+	if(num ==2){
+		return 1;
+	}
+		return fibonacci(num-1)+fibonacci(num-2);
 }
 
 module.exports = fibonacci;
